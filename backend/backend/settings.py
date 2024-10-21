39a1e3e8559a005812ff861a51f97c6fea6a92ae
Nithinton <nithinton@gmail.com>
2024-10-21 23:04:01 +0530

    Added Loggers in both console and file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,3 +121,36 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+        },
+         "file": {
+            "class": "logging.FileHandler",
+            "filename": "backend.log",
+            "formatter": "verbose",
+        }
+    },
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        }
+    },
+     "loggers": {
+        "": {
+            "handlers": ["console", "file"],
+            "level": "DEBUG",
+        }
+    }
+}
